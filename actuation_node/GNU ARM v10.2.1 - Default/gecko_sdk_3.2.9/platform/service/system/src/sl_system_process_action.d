6140d4fdde3c6f3431d6a487f538b11855382873
Ajay Kandagal <ajaykandagal94@gmail.com>
2023-12-02 15:40:47 -0700

    removing redundant files

--- actuation_node/GNU ARM v10.2.1 - Default/gecko_sdk_3.2.9/platform/service/system/src/sl_system_process_action.d
+++
@@ -1,6 +0,0 @@
-gecko_sdk_3.2.9/platform/service/system/src/sl_system_process_action.o: \
- C\:/SiliconLabs/SimplicityStudio/v5/developer/sdks/gecko_sdk_suite/v3.2/platform/service/system/src/sl_system_process_action.c \
- c\:\siliconlabs\simplicitystudio\v5\developer\sdks\gecko_sdk_suite\v3.2\platform\common\toolchain\inc\sl_gcc_preinclude.h \
- C\:\Users\ajayk\Desktop\Courses\LPEDT\lpedt-course-prj\actuation_node\autogen/sl_event_handler.h
-c\:\siliconlabs\simplicitystudio\v5\developer\sdks\gecko_sdk_suite\v3.2\platform\common\toolchain\inc\sl_gcc_preinclude.h:
-C\:\Users\ajayk\Desktop\Courses\LPEDT\lpedt-course-prj\actuation_node\autogen/sl_event_handler.h:
